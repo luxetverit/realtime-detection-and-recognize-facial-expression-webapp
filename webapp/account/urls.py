26c6c5a4d908d799be8873c82a7746e7e62734df
Lee jae young <117875597+JAYJAY1005@users.noreply.github.com>
2023-03-25 13:46:21 +0900

    Update urls.py

--- webapp/account/urls.py
+++ webapp/account/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path ,include
+from django.urls import path 
 from . import views
 
 app_name = "account"
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('', include('account.urls')),
 ]
