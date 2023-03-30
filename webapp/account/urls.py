514639739ec76f9ed9ff4ba285ba82c8e0a18140
JHS <117875637+zzoall@users.noreply.github.com>
2023-03-30 15:41:37 +0900

    commit

--- webapp/account/urls.py
+++ webapp/account/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path 
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 app_name = "account"
 
@@ -9,6 +11,10 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name="profile"),
     path('profile/userinfo/', views.userinfo, name="userinfo"),
-    path('password/', views.password, name='password'),
+    # 비밀번호 변경
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -18,9 +18,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path('app/', include('app.urls')),
     path('cam/',include('webcam_app.urls')),
     path('accounts/', include('account.urls')),
+    path('', include('django.contrib.auth.urls')),
 ]
 # from app.views import index, question_insert
 
