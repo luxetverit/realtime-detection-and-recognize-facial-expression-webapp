71264ab779772be353bbb6693e5c24013a46666d
JAE KYUNG <115970361+bmr03016@users.noreply.github.com>
2023-03-30 09:28:50 +0900

    Update urls.py

--- webapp/webcam_app/urls.py
+++ webapp/webcam_app/urls.py
@@ -6,11 +6,12 @@ app_name = "webcam"
 
 urlpatterns = [
     
-    path('', views.counseling_list, name='counseling_list'),
-    path('<int:pk>/', views.counseling_detail, name='counseling_detail'),
-    path('form/', views.counseling_add, name='counseling_add'),
-    path('edit/<int:pk>/', views.counseling_edit, name='counseling_edit'),
-    path('delete/<int:pk>/', views.counseling_delete, name='counseling_delete'),
-    path('cam', views.socket, name='cam'),
+    path('',views.index,name='index'),
+    path('list/', views.CounselingListView.as_view(), name='counseling_list'),
+    path('list/<int:pk>/', views.CounselingDetailView.as_view(), name='counseling_detail'),
+    path('list/form/', views.counseling_add, name='counseling_add'),
+    path('list/edit/<int:pk>/', views.counseling_edit, name='counseling_edit'),
+    path('list/delete/<int:pk>/', views.counseling_delete, name='counseling_delete'),
+    path('realtime/<int:pk>', views.socket, name='realtime'),
     
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
