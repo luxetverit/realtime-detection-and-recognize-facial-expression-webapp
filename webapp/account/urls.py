c9588848134a1619f9883111a1967a2bb2b2a0ab
Lee jae young <117875597+JAYJAY1005@users.noreply.github.com>
2023-03-30 20:34:23 +0900

    이메일로 비밀번호 재설정 기능 추가

--- webapp/account/urls.py
+++ webapp/account/urls.py
@@ -1,14 +1,23 @@
 from django.urls import path 
 from . import views
+from django.contrib.auth import views as auth_views
 
-app_name = "account"
+# 장고 django.contrib.auth.urls 사용으로 경로 재설정함 
+# app_name = "account"
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('profile/', views.profile, name="profile"),
-    path('profile/userinfo/', views.userinfo, name="userinfo"),
-    path('password/', views.password, name='password'),
+    # 장고 django.contrib.auth.urls 에서 기본으로 제공하는 유저 관리 폼
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    
+    # 장고 django.contrib.auth.urls 사용으로 경로에 'account' 재설정
+    path('account/signup/', views.signup, name='signup'),
+    path('account/login/', views.login_view, name='login'),
+    path('account/logout/', views.logout_view, name='logout'),
+    path('account/profile/', views.profile, name="profile"),
+    path('account/profile/userinfo/', views.userinfo, name="userinfo"),
+      
 ]
 
--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -153,3 +153,24 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# email 백엔드
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# 메일을 보내는 호스트 서버
+EMAIL_HOST = 'smtp.gmail.com'
+
+# ENAIL_HOST에 정의된 SMTP 서버가 사용하는 포트 (587: TLS/STARTTLS용 포트)
+EMAIL_PORT = '587'
+
+#  발신할 이메일 주소 
+EMAIL_HOST_USER = 'ijaeyeong429@gmail.com'
+
+# 발신할 이메일 비밀번호 (2단계 인증일경우 앱 비밀번호)
+EMAIL_HOST_PASSWORD = 'hvkjqtgmbspywklp'
+
+# TLS 보안 방법 (SMPT 서버와 통신할 떄 TLS (secure) connection 을 사용할지 말지 여부)
+EMAIL_USE_TLS = True
+
+# 사이트와 관련한 자동응답을 받을 이메일 주소
+DEFAULT_FROM_EMAIL = 'keol8740@naver.com'
