9b5f438da1b0c915511d065e7cbd89bd2873572a
JAE KYUNG <115970361+bmr03016@users.noreply.github.com>
2023-03-30 09:29:05 +0900

    Update admin.py

--- webapp/webcam_app/admin.py
+++ webapp/webcam_app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Counseling
+
+from .models import Counseling,DetectedEmotions,Videos
 
 # Register your models here.
 admin.site.register(Counseling)
+admin.site.register(DetectedEmotions)
+admin.site.register(Videos)
+
