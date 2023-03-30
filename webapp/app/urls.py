4780fae7d509a6c16580d4b05ac67e2425d4c391
JAE KYUNG <115970361+bmr03016@users.noreply.github.com>
2023-03-30 09:27:33 +0900

    Update urls.py

--- webapp/app/urls.py
+++ webapp/app/urls.py
@@ -7,10 +7,9 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('demo/', views.demo, name="demo"),
     path('service/', views.service, name="service"),
-    path('profile/', views.profile, name="profile"),
-    path('profile/userinfo/', views.userinfo, name="userinfo"),
-    path('password/', views.password, name='password'),
-    path('qna/', views.password, name='qna'),
     path('chart', views.line_chart, name='line_chart'),
     path('chartJSON', views.line_chart_json, name='line_chart_json'),
+    path('qna/', views.qna, name='qna'),
+    path('detail/', views.detail, name='detail'),
+    path('post/create/', views.Posts_create, name='posts_create'),
 ]
