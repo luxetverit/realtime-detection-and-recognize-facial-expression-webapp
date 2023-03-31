cbde8713e3f2bc7f553608b02cf645b4fa83d20f
jwnam <replaysoccer@naver.com>
2023-03-31 14:09:06 +0900

    update setting files

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = [
     STATIC_DIR,
     ]
 
-STATIC_ROOT = os.path.join(ROOT_DIR,'_static')
+STATIC_ROOT = os.path.join(BASE_DIR,'_static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
