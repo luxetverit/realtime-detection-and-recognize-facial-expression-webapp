c1ee95408e57bf9f32a26b08f07544d1ad6060a4
SeiHyunChoi <41310927+seok9924@users.noreply.github.com>
2023-04-02 17:36:30 +0900

    ws
    sw

--- webapp/webcam_app/routing.py
+++ webapp/webcam_app/routing.py
@@ -4,5 +4,5 @@ from . import consumers
 websocket_urlpatterns = [
     # re_path(r'ws/video/$', consumers.VideoConsumer.as_asgi()),
     re_path(r'ws/video/(?P<counseling_id>\d+)/$', consumers.VideoConsumer.as_asgi()),
-    re_path(r'wss/video/(?P<counseling_id>\d+)/$', consumers.VideoConsumer.as_asgi()),
+    # re_path(r'wss/video/(?P<counseling_id>\d+)/$', consumers.VideoConsumer.as_asgi()),
 ]
