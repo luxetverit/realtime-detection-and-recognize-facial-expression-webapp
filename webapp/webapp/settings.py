bd573b4a05f894993478c94051097f298ade6dd6
SeiHyunChoi <41310927+seok9924@users.noreply.github.com>
2023-04-03 14:12:30 +0900

    asdasd
    asdasdasd

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = DEV_SECRET['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 ALLOWED_HOSTS = ['emo-ai.com', 'localhost', 'www.emo-ai.com','10.0.0.245', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://emo-ai.com', 'https://www.emo-ai.com']
 # Application definition
@@ -43,6 +42,8 @@ INSTALLED_APPS = [
     'chartjs', #pip install django-chartjs
     'account',
     'post_comment',
+    'uvicorn',
+    'gunicorn',
 ]
 
 MIDDLEWARE = [
