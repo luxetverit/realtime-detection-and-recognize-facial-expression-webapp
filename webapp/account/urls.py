c6c069d8cebb433c0eda5d779013a0fd0f1f30a5
SeiHyunChoi <41310927+seok9924@users.noreply.github.com>
2023-04-06 14:22:38 +0900

    d
    d

--- webapp/account/urls.py
+++ webapp/account/urls.py
@@ -10,9 +10,4 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name="profile"),
     path('profile/userinfo/', views.userinfo, name="userinfo"),
-
-    path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -17,12 +17,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('post_comment.urls')),
     path('cam/',include('webcam_app.urls')),
     path('accounts/', include('account.urls')),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # from app.views import index, question_insert
 
