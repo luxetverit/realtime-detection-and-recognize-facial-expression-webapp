42e8c66187f579740be8a31f036eb9c04748c7b8
JAE KYUNG <115970361+bmr03016@users.noreply.github.com>
2023-04-06 15:08:51 +0900

    Update routing.py

--- webapp/webcam_app/routing.py
+++ webapp/webcam_app/routing.py
@@ -1,8 +1,15 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webapp.settings")
+
+import django
+django.setup()
+
 from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
     # re_path(r'ws/video/$', consumers.VideoConsumer.as_asgi()),
-    re_path(r'ws/video/(?P<counseling_id>\d+)/$', consumers.VideoConsumer.as_asgi()),
+    re_path(r'ws/video/(?P<counseling_id>\d+)/$', consumers.ImageConsumer.as_asgi()),
     # re_path(r'wss/video/(?P<counseling_id>\d+)/$', consumers.VideoConsumer.as_asgi()),
 ]
+
